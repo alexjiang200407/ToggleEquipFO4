e1d267680f1ee8c7f84eff095e5362856229894c
alex jiang <alexjiang201507@gmail.com>
2024-05-14 12:29:42 +1000

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <spdlog/sinks/basic_file_sink.h>
+
+void InitializeLog()
+{
+	auto path = logger::log_directory();
+	if (!path) {
+		F4SE::stl::report_and_fail("Failed to find standard logging directory"sv);
+	}
+
+	*path /= Version::PROJECT;
+	*path += ".log"sv;
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::info);
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("[%H:%M:%S:%e] %v"s);
+
+	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
+}
+
+
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface* a_f4se, F4SE::PluginInfo* a_info)
+{
+	a_info->infoVersion = F4SE::PluginInfo::kVersion;
+	a_info->name = Version::PROJECT.data();
+	a_info->version = Version::MAJOR;
+
+	if (a_f4se->IsEditor()) {
+		logger::critical("loaded in editor");
+		return false;
+	}
+
+	const auto ver = a_f4se->RuntimeVersion();
+	if (ver < F4SE::RUNTIME_1_10_162) {
+		logger::critical("unsupported runtime v{}", ver.string());
+		return false;
+	}
+
+	return true;
+}
+
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_f4se)
+{
+	F4SE::Init(a_f4se);
+	InitializeLog();
+	logger::info("hello world!");
+
+	return true;
+}
